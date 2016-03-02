e5afff3e788c0185856692a53e69af6547350e58
Leonardo <leonardo.batista4@fatec.sp.gov.br>
2016-03-02 08:40:59 -0300

    Added files via upload

---
+++ ex-prof.js
@@ -0,0 +1,9 @@
+function gerarListener(){
+	var obj;
+	var count = 0;
+
+	return obj;
+}
+
+var countdb = gerarListener();
+
---
+++ slide5.js
@@ -0,0 +1,11 @@
+var evento;
+var cont = 0;
+var.contador = funcion{
+	++cont;
+}
+
+
+function imprimir(evento){
+	console.log("Ola")
+	console.log(var.contador);
+}
