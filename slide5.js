0e00e257997d17c2b63cfb21131cee7c6546682c
Leonardo <leonardo.batista4@fatec.sp.gov.br>
2016-03-04 19:46:46 -0300

    Continuação da atividade de Observer
    Corrigidos alguns errors

--- slide5.js
+++ slide5.js
@@ -1,11 +1,55 @@
-var evento;
-var cont = 0;
-var.contador = funcion{
-	++cont;
+function construtor(){
+
+	
+	var observer = {};
+	var listaObservers = [];
+	var contador = 0;
+
+	var gatilhoscont = 0;
+
+	observer.gatilho=function(){
+		
+		for(var x=0; x<listaObservers.length; x++){
+			var funcao = listaObservers[x];
+			funcao();
+			++gatilhoscont;
+			console.log("gatilho ativado "+gatilhoscont+" vez(es)")
+		}
+	}
+
+	observer.contar = function(){
+		++contador;
+	}
+
+	observer.exibeContador = function(){
+		return contador;
+	}
+
+	observer.add = function(objeto){
+		listaObservers.push(objeto);
+	}
+
+	return observer;
 }
 
 
-function imprimir(evento){
-	console.log("Ola")
-	console.log(var.contador);
+function exibirContador(){
+	console.log(observer.exibeContador());
 }
+
+
+function greetings(){
+	console.log("Hello world");
+}
+
+
+
+
+var observer = construtor();
+observer.add(exibirContador);
+observer.add(greetings);
+observer.contar();
+observer.contar();
+observer.contar();
+observer.contar();
+observer.gatilho();
